e4ac03b027bab4ff3502de96edae6e23faeec25f
Keerthi3275 <70685704+Keerthi3275@users.noreply.github.com>
2020-11-19 13:13:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var engine,world;
 function setup() 
 {
-  createCanvas(800,400);
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  object=Bodies.rectangle(200,100,50,50);
 }
 
 function draw() 
